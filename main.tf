966b21b928e3f9c96c7b3b6ab68b42ecea8f8936
Tyler Lynch <=>
2022-06-13 13:41:50 -0400

    Updated service namespace

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ data "aws_iam_policy_document" "kendra_index_cloudwatch_policy" {
     condition {
       test     = "StringEquals"
       variable = "cloudwatch:namespace"
-      values   = ["Kendra"]
+      values   = ["AWS/Kendra"]
     }
   }
   statement {
